8d85f90a25cabc8405f55f0de1e120c058ccb3ab
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-06-29 00:08:54 +0300

    functions lab continue

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+//Testing the function
+console.log(saturdayFun()); //"This Saturday, I want to roller-skate!"
+console.log(saturdayFun("bathe my dog")); //"This Saturday, I want to bathe my dog!"
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Testing the function
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  // Testing the function
+  const encouragingPromptFunction = wrapAdjective("*");
+  console.log(encouragingPromptFunction("a hard worker")); // "You are *a hard worker*!"
+  console.log(wrapAdjective("||")("a dedicated programmer")); // "You are ||a dedicated programmer||!"
+  
